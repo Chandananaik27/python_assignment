093d645eaf104bc695f4bc6ddc6a8a0e5ac06802
Your Full Name <your@email.com>
2025-06-08 17:31:02 +0530

    second_maximum_in_list

---
+++ src/second_maximum_in_list/driver.py
@@ -0,0 +1,5 @@
+from util import second_maximum
+
+# num = int(input("Enter the number of values: "))
+# List_Input = list(map(int, input("Enter the values: ").split()))
+second_maximum()
---
+++ src/second_maximum_in_list/util.py
@@ -0,0 +1,14 @@
+import logging
+from typing import Any
+
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+def second_maximum() -> Any:
+    num = int(input("Enter the number of values: "))
+    List = list(map(int, input("Enter the values: ").split()))
+    for i in range(len(List)):
+        for j in range(i + 1, len(List)):
+            if List[i] > List[j]:
+                List[i], List[j] = List[j], List[i]
+    second_max=List[-2]
+    logging.debug(f"The second largest value is {second_max}")
+    return second_max
