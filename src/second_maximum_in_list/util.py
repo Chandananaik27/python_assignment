e17f626f8163c85f28622a0386ac029016a83206
Your Full Name <your@email.com>
2025-06-09 10:12:45 +0530

    second_max

--- src/second_maximum_in_list/util.py
+++ src/second_maximum_in_list/util.py
@@ -2,13 +2,13 @@ import logging
 from typing import Any
 
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
-def second_maximum() -> Any:
-    num = int(input("Enter the number of values: "))
-    List = list(map(int, input("Enter the values: ").split()))
+
+def second_maximum(num: int, List: list[int]) -> Any:
+    # Sort the list in ascending order
     for i in range(len(List)):
         for j in range(i + 1, len(List)):
             if List[i] > List[j]:
                 List[i], List[j] = List[j], List[i]
-    second_max=List[-2]
+    second_max = List[-2]  # Second last value after sorting
     logging.debug(f"The second largest value is {second_max}")
     return second_max
