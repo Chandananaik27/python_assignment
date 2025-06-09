f0d163c31b472b2735038939dca395aa0e62acdb
Your Full Name <your@email.com>
2025-06-09 10:30:49 +0530

    string_formatting

--- src/string_formatting/util.py
+++ src/string_formatting/util.py
@@ -2,15 +2,19 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
-def string_formatting():
-    k = int(input("Enter the number: "))
+def string_formatting(k: int) -> str:
+    if k < 1:
+        return ""
+
     de = [str(i) for i in range(1, k + 1)]
     oc = [oct(i)[2:] for i in range(1, k + 1)]
     he = [hex(i)[2:].upper() for i in range(1, k + 1)]
     bi = [bin(i)[2:] for i in range(1, k + 1)]
 
+    width = len(bi[-1])  # Width based on largest binary number
     res = ""
     for i in range(k):
-        res += f"{de[i]:>2}{' ':>5}{oc[i]:>3}{' ':>5}{he[i]:>3}{' ':>5}{bi[i]:>3}\n"
+        res += f"{de[i]:>{width}} {oc[i]:>{width}} {he[i]:>{width}} {bi[i]:>{width}}\n"
+
     logging.debug(res)
     return res
