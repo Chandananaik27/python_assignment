d52ccf18f2b2e9726b93c986e9768e5aa9fbd162
Your Full Name <your@email.com>
2025-06-09 09:56:37 +0530

    mutation

--- src/mutations/util.py
+++ src/mutations/util.py
@@ -1,8 +1,7 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format="%(message)s" )
-def mutate_string():
-    string = input("Enter the string: ")
-    position, character = input("enter the position and character: ").split()
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+
+def mutate_string(string, position, character):
     string_input = string[:position] + character + string[position + 1:]
     logging.debug(string_input)
     return string_input
